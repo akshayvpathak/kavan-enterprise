ed5ff9386a0d1b6113dd8edd3a932f94eeebf9a8
Akshay Pathak <akshayvpathak@yahoo.com>
2024-07-25 00:44:36 +0530

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -75,7 +75,7 @@ export default function RootLayout({ children }: RootLayoutProps) {
       >
         <ThemeProvider
           attribute="class"
-          defaultTheme="system"
+          defaultTheme="light"
           enableSystem
           disableTransitionOnChange
         >
